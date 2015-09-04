6688c1c42414d7a698c7c0a552fcbd906ec715cc
OtavioRMachado <omachado@thoughtworks.com>
2015-09-04 19:00:28 -0300

    @otaviormachado | @josigama | @vvgomes - wip #9 - Removing unused dependencies

--- src/main/java/br/com/registrolivre/Application.java
+++ src/main/java/br/com/registrolivre/Application.java
@@ -1,14 +1,8 @@
 package br.com.registrolivre;
 
-import br.com.registrolivre.configurations.DataSourceConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication(exclude = FlywayAutoConfiguration.class)
 public class Application {
