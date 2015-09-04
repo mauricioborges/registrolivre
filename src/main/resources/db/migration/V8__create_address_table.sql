37d0e0ed8fcb421d7170362163d7600ba0cc518d
OtavioRMachado <omachado@thoughtworks.com>
2015-09-04 18:55:40 -0300

    @otaviormachado | @josigama - wip #9 - Creating migrations for the database.

--- src/main/resources/db/migration/V8__create_address_table.sql
+++ src/main/resources/db/migration/V8__create_address_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS address (
-    id   SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     company_id INT NOT NULL REFERENCES companies(id),
     street_name VARCHAR(255),
-    number INT,
+    number VARCHAR(255),
     complement VARCHAR(255),
     state VARCHAR(255),
     city VARCHAR(255),
--- src/test/resources/db_file.sql
+++ src/test/resources/db_file.sql
@@ -2,7 +2,13 @@ CREATE TABLE IF NOT EXISTS companies (
   id   SERIAL PRIMARY KEY,
   cnpj VARCHAR(18) UNIQUE NOT NULL,
   trade_name VARCHAR(150),
-  company_name varchar(150) DEFAULT NULL
+  company_name VARCHAR(150) DEFAULT NULL,
+  street_name VARCHAR(255) DEFAULT NULL,
+  number VARCHAR(255) DEFAULT NULL,
+  complement VARCHAR(255) DEFAULT NULL,
+  state VARCHAR(255) DEFAULT NULL,
+  city VARCHAR(255) DEFAULT NULL,
+  cep VARCHAR(9) DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS documents (
@@ -11,19 +17,8 @@ CREATE TABLE IF NOT EXISTS documents (
     url VARCHAR(500)
 );
 
-CREATE TABLE IF NOT EXISTS address (
-    id   SERIAL PRIMARY KEY,
-    company_id INT NOT NULL REFERENCES companies(id),
-    street_name VARCHAR(255),
-    number INT,
-    complement VARCHAR(255),
-    state VARCHAR(255),
-    city VARCHAR(255),
-    cep VARCHAR(9)
-);
-
-INSERT INTO companies VALUES (1, '40.573.872/0001-63', 'Marcenaria Tio Zé', 'Marcenaria LTDA Brasil');
-INSERT INTO companies VALUES (2, '40.573.872/0001-60', 'Marcenaria Tio João', 'Marcenaria LTDA Argentina');
+INSERT INTO companies VALUES (1, '40.573.872/0001-63', 'Marcenaria Tio Zé', 'Marcenaria LTDA Brasil', null, null, null, null, null, null);
+INSERT INTO companies VALUES (2, '40.573.872/0001-60', 'Marcenaria Tio João', 'Marcenaria LTDA Argentina', null, null, null, null, null, null);
 
 CREATE SEQUENCE hibernate_sequence INCREMENT 1 START 3;
 
