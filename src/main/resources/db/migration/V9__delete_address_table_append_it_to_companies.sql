6d5da32f0149789a59728b3c9d38f14484205c8e
OtavioRMachado <omachado@thoughtworks.com>
2015-09-09 17:50:28 -0300

    @otaviormachado | @josigama - wip #9 - Created character limitations for Company Registration inputs.

--- src/main/resources/db/migration/V9__delete_address_table_append_it_to_companies.sql
+++ src/main/resources/db/migration/V9__delete_address_table_append_it_to_companies.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS address;
 ALTER TABLE companies ADD COLUMN street_name varchar(255) DEFAULT NULL;
-ALTER TABLE companies ADD COLUMN number varchar(255) DEFAULT NULL;
-ALTER TABLE companies ADD COLUMN complement varchar(255) DEFAULT NULL;
-ALTER TABLE companies ADD COLUMN state varchar(255) DEFAULT NULL;
-ALTER TABLE companies ADD COLUMN city varchar(255) DEFAULT NULL;
-ALTER TABLE companies ADD COLUMN cep varchar(255) DEFAULT NULL;
+ALTER TABLE companies ADD COLUMN number varchar(10) DEFAULT NULL;
+ALTER TABLE companies ADD COLUMN complement varchar(20) DEFAULT NULL;
+ALTER TABLE companies ADD COLUMN state varchar(2) DEFAULT NULL;
+ALTER TABLE companies ADD COLUMN city varchar(60) DEFAULT NULL;
+ALTER TABLE companies ADD COLUMN cep varchar(9) DEFAULT NULL;
