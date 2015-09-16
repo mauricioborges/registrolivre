cf9615dd4ba15f4d32c9fa74a15ff2d31a195c18
Rodrigo Morais <morais.rm@gmail.com>
2015-09-16 16:04:35 -0300

    @rodrigo-morais @otaviormachado - #15 - Adding a task to create register keys.

---
+++ infrastructure/application/scripts/provision/create-register-keys.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+echo #creating keys
+
+root=$(pwd)/../..
+
+rm -rf .keys
+mkdir .keys
+cd .keys
+
+ssh-keygen -t rsa -N "" -b 4096 -C "registrolivre" -f registrolivre
+
+cp registrolivre.pub ../public_keys/
+
+if [ -z "$(cat ${root}/.profile | grep REGISTROLIVRE_PRIVATE_KEY)" ]; then
+    echo 'export REGISTROLIVRE_PRIVATE_KEY="${root}/infrastructure/application/registrolivre"' | tee -a ${root}/.profile
+fi
+
+if [ -z "$(cat ${root}/.profile | grep LOCAL_REGISTROLIVRE_IP)" ]; then
+    echo 'export LOCAL_REGISTROLIVRE_IP="192.168.33.71"' | tee -a ${root}/.profile
+fi
+
+source ${root}/.profile
