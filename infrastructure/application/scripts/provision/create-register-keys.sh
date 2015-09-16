1a6141235a34fb59614a4e3f047b1c80b646d8dc
Rodrigo Morais <morais.rm@gmail.com>
2015-09-16 18:55:32 -0300

    @rodrigo-morais @otaviormachado - #15 - Adjusting task to create environment variables to copy key in correct path.

--- infrastructure/application/scripts/provision/create-register-keys.sh
+++ infrastructure/application/scripts/provision/create-register-keys.sh
@@ -4,8 +4,6 @@ set -e
 
 echo #creating keys
 
-root=$(pwd)/../..
-
 rm -rf .keys
 mkdir .keys
 cd .keys
@@ -14,12 +12,12 @@ ssh-keygen -t rsa -N "" -b 4096 -C "registrolivre" -f registrolivre
 
 cp registrolivre.pub ../public_keys/
 
-if [ -z "$(cat ${root}/.profile | grep REGISTROLIVRE_PRIVATE_KEY)" ]; then
-    echo 'export REGISTROLIVRE_PRIVATE_KEY=$(pwd)/infrastructure/application/registrolivre' | tee -a ${root}/.profile
+if [ -z "$(cat ~/.registro-livre-profile | grep REGISTROLIVRE_PRIVATE_KEY)" ]; then
+    echo 'export REGISTROLIVRE_PRIVATE_KEY=$(pwd)/infrastructure/application/.keys/registrolivre' | tee -a ~/.registro-livre-profile
 fi
 
-if [ -z "$(cat ${root}/.profile | grep LOCAL_REGISTROLIVRE_IP)" ]; then
-    echo 'export LOCAL_REGISTROLIVRE_IP="192.168.33.71"' | tee -a ${root}/.profile
+if [ -z "$(cat ~/.registro-livre-profile | grep LOCAL_REGISTROLIVRE_IP)" ]; then
+    echo 'export LOCAL_REGISTROLIVRE_IP="192.168.33.71"' | tee -a ~/.registro-livre-profile
 fi
 
-source ${root}/.profile
+source ~/.registro-livre-profile
