357dee6e9ee9fc173b61503c30a88b8eb979a540
Rodrigo Morais <morais.rm@gmail.com>
2015-09-16 10:52:54 -0300

    @rodrigo-morais - #15 - Adjusting the task which import environment variables to AWS, using a variable to profile file.

--- infrastructure/application/scripts/provision/create-environment-variables.sh
+++ infrastructure/application/scripts/provision/create-environment-variables.sh
@@ -9,4 +9,4 @@ ID=$REGISTROLIVRE_AWS_ACCESS_KEY_ID
 echo "=====> AWS variables"
 ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "echo 'export REGISTROLIVRE_AWS_ACCESS_KEY_ID=${ID}' | tee -a ${USER}/.profile"
 ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "echo 'export REGISTROLIVRE_AWS_SECRET_ACCESS_KEY=${SECRET}' | tee -a ${USER}/.profile"
-ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "source /home/registrolivre/.profile"
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@$LOCAL_REGISTROLIVRE_IP "source ${USER}/.profile"
