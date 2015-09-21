8ebab907b5a844553dbfafe174c46afef7b52fa4
OtavioRMachado <omachado@thoughtworks.com>
2015-09-21 14:34:02 -0300

    @otaviormachado | @pedrro - wip #17 - Fixing sql files.

--- src/test/resources/db_file.sql
+++ src/test/resources/db_file.sql
@@ -1,11 +1,1 @@
-CREATE TABLE IF NOT EXISTS companies (
-  id   SERIAL PRIMARY KEY,
-  cnpj VARCHAR(18) UNIQUE NOT NULL,
-  trade_name VARCHAR(150)
-);
-
-INSERT INTO companies VALUES (1, '40.573.872/0001-63', 'Marcenaria Tio Zé');
-INSERT INTO companies VALUES (2, '40.573.872/0001-60', 'Marcenaria Tio João');
-
-CREATE SEQUENCE hibernate_sequence INCREMENT BY 1 START WITH 3;
-
+CREATE SEQUENCE hibernate_sequence INCREMENT BY 1 START WITH 1;
