e53753e8e82961e61fda78cd0fc3233ea35e468b
OtavioRMachado <omachado@thoughtworks.com>
2015-09-23 14:42:55 -0300

    @otaviormachado | @pedrro - Refactoring suggested by Duda - extract conditional in a method.

--- src/main/resources/static/js/directives/date-validation-directive.js
+++ src/main/resources/static/js/directives/date-validation-directive.js
@@ -4,13 +4,16 @@ app.directive("dateValidation", [function() {
     require: 'ngModel',
     link: function(scope, element, attr, ngModel) {
 
-        var dateValidator = function(inputDate) {
-            console.log(inputDate);
+        var dateExists = function(inputDate) {
             var dateArray = inputDate.split("/");
             var dateStandard = dateArray[2]+ "/"+ dateArray[1]+ "/" + dateArray[0];
             var date = new Date(dateStandard);
 
-            if (inputDate.length == 0 || date.getDate()==dateArray[0] && (date.getMonth()+1)==dateArray[1] && date.getFullYear()==dateArray[2]) {
+            return inputDate.length == 0 || date.getDate()==dateArray[0] && (date.getMonth()+1)==dateArray[1] && date.getFullYear()==dateArray[2];
+        }
+
+        var dateValidator = function(inputDate) {
+            if (dateExists(inputDate)) {
                 return true;
             } else {
                 return false;
