2dd88bb755ce8be6595b36a7f9f4eee7a4b8527a
Guilherme Dias <gdias@thoughtworks.com>
2015-09-28 18:51:47 -0300

    @otaviormachado @guilhermedias #24 Drop and re-create entire database before running functional tests

--- infrastructure/application/scripts/tests/functional/functional-test.sh
+++ infrastructure/application/scripts/tests/functional/functional-test.sh
@@ -1,4 +1,5 @@
-psql -d registro_livre -c "drop table if exists documents cascade; drop table if exists companies cascade;"
+psql -d registro_livre -c "drop database if exists registro_livre;"
+psql -d registro_livre -c "create database registro_livre with owner registro_livre_user;"
 gradle copyVendorFiles bootRun &
 sleep 30
 psql -d registro_livre -c "insert into companies(cnpj, company_name, trade_name, id) values('81.746.232/0001-95', 'Test CNPJ', 'Test CNPJ', 99999);"
