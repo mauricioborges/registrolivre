8fa6e9b77169afbf153ef2cd13ee95337965ef0b
Pedro Silva <pedrro2@hotmail.com>
2015-10-02 18:47:41 -0300

    @pedrro - adding scripts to go agent

--- infrastructure/application/scripts/tests/functional/functional-test.sh
+++ infrastructure/application/scripts/tests/functional/functional-test.sh
@@ -1,9 +1,21 @@
-psql -c "drop database if exists registro_livre;"
-psql -c "create database registro_livre with owner registro_livre_user;"
+#!/bin/sh
+
+# Drop and create database
+psql -U postgres -c "drop database if exists registro_livre;"
+psql -U postgres -c "create database registro_livre with owner registro_livre_user;"
+
+# Execute registro livre application
+cd registrolivre
 ./gradlew copyVendorFiles bootRun 2>&1 &
+
+# sleep
 sleep 30
+
+# start webdriver server
 webdriver-manager start > /dev/null 2>&1 &
 sleep 5
+
+# execute test
 protractor src/test/resources/functional/firefox-conf.js
 sleep 5
 curl "http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer"
