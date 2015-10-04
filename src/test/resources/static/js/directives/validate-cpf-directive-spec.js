fbc0182477097b1adaeb58dad284b0300aec096c
Pedro Silva <pedrro2@hotmail.com>
2015-10-04 18:07:53 -0300

    @pedrro - #28 - Creating cpf-directive-spec to add tests

---
+++ src/test/resources/static/js/directives/validate-cpf-directive-spec.js
@@ -0,0 +1,19 @@
+describe('Directive: cpf-directive', function() {
+ var $rootScope, $scope;
+ var validCPF = "965.416.843-08";
+ var invalidCPF = "100.000.000-00";
+
+    beforeEach(module('validate-cpf'));
+    
+    beforeEach(inject(function($injector) {
+
+     }));
+
+    it('Should return true to a valid cpf', function(){
+
+    });
+
+    it('Should return false to a invalid cpf', function(){
+
+    });
+});
