5721062685b05d3726245c1ea58f0951691ccbeb
Eduardo Braga <ebfjunior@gmail.com>
2015-10-06 14:56:47 -0300

    @ebragaparah | @pedrro - #28 - added unit tests to cpf-validation directive.

--- src/test/resources/static/js/directives/validate-cpf-directive-spec.js
+++ src/test/resources/static/js/directives/validate-cpf-directive-spec.js
@@ -1,19 +1,36 @@
-describe('Directive: cpf-directive', function() {
- var $rootScope, $scope;
- var validCPF = "965.416.843-08";
- var invalidCPF = "100.000.000-00";
+describe('Directive: validate-cpf', function() {
+    var element, scope;
+    var VALID_CPF = "549.741.127-40";
+    var INVALID_CPF = "100.000.000-00";
 
+    partner = {
+        cpf: "000.000.000-00",
+    };
+
+    beforeEach(module('registro-livre'));
     beforeEach(module('validate-cpf'));
     
-    beforeEach(inject(function($injector) {
+    beforeEach(inject(function($rootScope, $compile) {
+         validateCPFHTML = '<input type="text" class="form-control" data-ng:model="partner.cpf" '+
+                           'ui-mask="999.999.999-99" name="cpf" id="cpf" validate-cpf />';
 
-     }));
+         scope = $rootScope;
+         scope.partner = partner;
+         element = $compile(validateCPFHTML)(scope);
+         scope.$digest();
+    }));
 
-    it('Should return true to a valid cpf', function(){
-
+    it('should accept valid cpf', function() {
+        scope.partner.cpf = VALID_CPF;
+        scope.$digest();
+        element.triggerHandler('blur');
+        element.hasClass('ng-invalid').should.be.false;
     });
 
-    it('Should return false to a invalid cpf', function(){
-
+    it('should NOT accept invalid cpf', function() {
+        scope.partner.cpf = INVALID_CPF;
+        scope.$digest();
+        element.triggerHandler('blur');
+        element.hasClass('ng-invalid').should.be.true;
     });
 });
