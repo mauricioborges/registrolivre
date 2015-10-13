7cf0d44f1d3b494c6728ffe742e1c1b09f52546e
Eduardo Braga <ebfjunior@gmail.com>
2015-10-13 11:04:13 -0300

    @ebragaparah | @dieselpardal - #27 - added a custom query to search companies by name

--- src/main/java/br/com/registrolivre/repository/CompanyRepository.java
+++ src/main/java/br/com/registrolivre/repository/CompanyRepository.java
@@ -1,7 +1,9 @@
 package br.com.registrolivre.repository;
 
 import br.com.registrolivre.models.Company;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +11,7 @@ import java.util.List;
 @Repository
 public interface CompanyRepository extends CrudRepository<Company, Long> {
     Company getByCnpj(String cnpj);
-    List<Company> getByTradeName(String tradeName);
+
+    @Query("select company from Company company where company.tradeName like %:tradeName%")
+    List<Company> getByTradeName(@Param("tradeName") String tradeName);
 }
