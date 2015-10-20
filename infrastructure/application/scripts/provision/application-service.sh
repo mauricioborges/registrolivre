8015636eb02c6f84b0f140d80a93a63c1c3c080a
Josi Gama <josyvisa@gmail.com>
2015-10-20 19:35:20 -0200

    @josigama @deniscosta @lipemorais #40 improve start up script

--- infrastructure/application/scripts/provision/application-service.sh
+++ infrastructure/application/scripts/provision/application-service.sh
@@ -1,15 +1,6 @@
-#!/bin/sh
-### BEGIN INIT INFO
-# Provides:          registrolivre
-# Required-Start:    $local_fs $remote_fs $network $syslog
-# Required-Stop:     $local_fs $remote_fs $network $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# X-Interactive:     true
-# Short-Description: Start/stop/Restart registrolivre server
-### END INIT INFO
+#!/bin/bash
 
-get_app_pid() {
+function get_app_pid {
     echo $(ps aux | \
         grep registrolivre.jar | \
         grep -v grep | \
@@ -17,7 +8,7 @@ get_app_pid() {
         cut -d ' ' -f 2)
 }
 
-is_app_running() {
+function is_app_running {
     if ps aux | grep registrolivre.jar | grep -v grep; then
         return 0
     fi
@@ -25,43 +16,45 @@ is_app_running() {
     return 1
 }
 
+function start_app {
+    if ! is_app_running; then
+        echo "Starting registrolivre service ..."
+        echo "Loading environment variables"
+        . /home/registrolivre/.profile
+
+        nohup java -Dserver.port=5000 -jar /home/registrolivre/app/registrolivre.jar \
+            /home/registrolivre/app \
+            2>> /home/registrolivre/app/logs.txt \
+            >> /home/registrolivre/app/logs.txt &
+
+        echo "registrolivre service started ..."
+    else
+        echo "registrolivre service is already running ..."
+    fi
+}
+
+function stop_app {
+    if is_app_running; then
+        echo "Stopping registrolivre service ..."
+        kill $(get_app_pid);
+        echo "registrolivre service stopped ..."
+    else
+        echo "registrolivre service is not running ..."
+    fi
+}
+
 case $1 in
     start)
-        if ! is_app_running; then
-            echo "Starting registrolivre service ..."
-            echo "Loading environment variables"
-            . /home/registrolivre/.profile
-            nohup java -Dserver.port=5000 -jar /home/registrolivre/app/registrolivre.jar /home/registrolivre/app 2>> /home/registrolivre/app/logs.txt >> /home/registrolivre/app/logs.txt &
-            echo "registrolivre service started ..."
-        else
-            echo "registrolivre service is already running ..."
-        fi
+        start_app
     ;;
     stop)
-        if is_app_running; then
-            echo "Stopping registrolivre service ..."
-            kill $(get_app_pid);
-            echo "registrolivre service stopped ..."
-        else
-            echo "registrolivre service is not running ..."
-        fi
+        stop_app
     ;;
     restart)
-        if [ -f /home/registrolivre/app/pid ]; then
-            PID=$(cat /home/registrolivre/app/pid);
-            echo "Stopping registrolivre service ...";
-            kill $PID;
-            echo "registrolivre service stopped ...";
-            rm /home/registrolivre/app/pid
-
-            echo "Starting registrolivre service ..."
-             echo "Loading environment variables"
-            . /home/registrolivre/.profile
-            nohup java -Dserver.port=5000 -jar /home/registrolivre/app/registrolivre.jar /home/registrolivre/app 2>> /home/registrolivre/app/logs.txt >> /home/registrolivre/app/logs.txt &
-            echo $! > /home/registrolivre/app/pid
-            echo "registrolivre service started ..."
-        else
-            echo "registrolivre service is not running ..."
-        fi
+        stop_app
+        while is_app_running; do
+            sleep 5
+        done
+        start_app
     ;;
 esac
