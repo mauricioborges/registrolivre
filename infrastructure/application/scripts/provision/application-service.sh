fa552d856a5a466f9866f13ea1fdc44cc8341fc0
Felipe de Morais <fmorais@thoughtworks.com>
2015-10-20 15:41:14 -0200

    @josigama @lipemorais #40 - fix the start and stop service script

--- infrastructure/application/scripts/provision/application-service.sh
+++ infrastructure/application/scripts/provision/application-service.sh
@@ -9,26 +9,39 @@
 # Short-Description: Start/stop/Restart registrolivre server
 ### END INIT INFO
 
+get_app_pid() {
+    echo $(ps aux | \
+        grep registrolivre.jar | \
+        grep -v grep | \
+        sed 's/ \+/ /g'| \
+        cut -d ' ' -f 2)
+}
+
+is_app_running() {
+    if ps aux | grep registrolivre.jar | grep -v grep; then
+        return 0
+    fi
+
+    return 1
+}
+
 case $1 in
     start)
-        if [ ! -f /home/registrolivre/app/pid ]; then
+        if ! is_app_running; then
             echo "Starting registrolivre service ..."
             echo "Loading environment variables"
             . /home/registrolivre/.profile
             nohup java -Dserver.port=5000 -jar /home/registrolivre/app/registrolivre.jar /home/registrolivre/app 2>> /home/registrolivre/app/logs.txt >> /home/registrolivre/app/logs.txt &
-            echo $! > /home/registrolivre/app/pid
             echo "registrolivre service started ..."
         else
             echo "registrolivre service is already running ..."
         fi
     ;;
     stop)
-        if [ -f /home/registrolivre/app/pid ]; then
-            PID=$(cat /home/registrolivre/app/pid);
+        if is_app_running; then
             echo "Stopping registrolivre service ..."
-            kill $PID;
+            kill $(get_app_pid);
             echo "registrolivre service stopped ..."
-            rm /home/registrolivre/app/pid
         else
             echo "registrolivre service is not running ..."
         fi
