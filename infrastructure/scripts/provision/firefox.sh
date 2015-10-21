5448137b384a7635693644b3b41748261c381bf6
Guilherme Dias <gdias@thoughtworks.com>
2015-10-21 13:36:47 -0200

    @guilhermedias #33 enable wget quiet mode

--- infrastructure/scripts/provision/firefox.sh
+++ infrastructure/scripts/provision/firefox.sh
@@ -4,7 +4,7 @@ set -e
 
 sudo su <<BLOCK
     echo "=====> Installing Firefox 34.0.5"
-    wget ftp.mozilla.org/pub/mozilla.org/firefox/releases/34.0.5/linux-x86_64/en-US/firefox-34.0.5.tar.bz2
+    wget ftp.mozilla.org/pub/mozilla.org/firefox/releases/34.0.5/linux-x86_64/en-US/firefox-34.0.5.tar.bz2 --quiet
     tar -xjvf firefox-34.0.5.tar.bz2
     mv firefox /opt/firefox
     ln -sf /opt/firefox/firefox /usr/bin/firefox
