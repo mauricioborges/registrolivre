415a3665a845bc01fa69cc57e81454d9865a5e5f
Guilherme Dias <gdias@thoughtworks.com>
2015-10-21 10:27:50 -0200

    @guilhermedias #33 Dowgrade Firefox to solve compatibility issue with Selenium

--- infrastructure/scripts/provision/firefox.sh
+++ infrastructure/scripts/provision/firefox.sh
@@ -3,9 +3,10 @@
 set -e
 
 sudo su <<BLOCK
-    echo "=====> Installing Firefox"
-    dpkg --configure -a
-    add-apt-repository --yes ppa:ubuntu-mozilla-security/ppa
-    apt-get update
-    apt-get --yes install firefox
+    echo "=====> Installing Firefox 34.0.5"
+    wget ftp.mozilla.org/pub/mozilla.org/firefox/releases/34.0.5/linux-x86_64/en-US/firefox-34.0.5.tar.bz2
+    tar -xjvf firefox-34.0.5.tar.bz2
+    mv firefox /opt/firefox
+    ln -sf /opt/firefox/firefox /usr/bin/firefox
+    rm firefox-34.0.5.tar.bz2
 BLOCK
