d476866f73860e7a5d8338e3ef1ab5cd90873b6f
Pedro Silva <pedrro2@hotmail.com>
2015-10-21 11:23:55 -0200

    @pedrro - #27 - Refactoring on css to adjust new id

--- src/test/resources/functional/pageObject/search.js
+++ src/test/resources/functional/pageObject/search.js
@@ -2,7 +2,7 @@ var Search = (function () {
     function Search(){}
 
     Search.prototype.fillSearchField = function(company) {
-          element(by.id("searchInput")).sendKeys(company);
+          element(by.id("search-input")).sendKeys(company);
     };
      Search.prototype.submitSearch = function() {
           element(by.id("btn-submit")).click();
