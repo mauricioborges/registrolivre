14bb08334069d005d9f68431a30ff0ec2600906b
Guilherme Dias <gdias@thoughtworks.com>
2015-10-22 15:56:03 -0200

    @guilhermedias @pedrro - #33 - Modifying functional test script to connect on VM

--- infrastructure/application/scripts/tests/functional/functional-test.sh
+++ infrastructure/application/scripts/tests/functional/functional-test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@192.168.33.71 << EOF
 
 clean_database() {
   sudo -u postgres psql registro_livre -c "delete from partners; delete from documents; delete from companies;"
@@ -13,3 +14,5 @@ xvfb-run protractor src/test/resources/functional/firefox-conf.js
 
 echo "Removing data created by functional tests"
 clean_database
+
+EOF
