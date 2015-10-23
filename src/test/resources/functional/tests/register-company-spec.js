4f92c436254a54af98be6662807cdf732ea57404
Eduardo Braga <ebfjunior@gmail.com>
2015-10-23 18:20:13 -0200

    @ebragaparah, @pedrro - changed identation for 4 spaces.

--- src/test/resources/functional/tests/register-company-spec.js
+++ src/test/resources/functional/tests/register-company-spec.js
@@ -5,30 +5,29 @@ var pdf;
 var newCompanyForm = new NewCompanyForm();
 
 beforeEach(function() {
-      pdf = require('path').resolve('./src/test/resources/file_uploader_functional_test.pdf');
-      browser.get('http://localhost:5000/#/cadastro');
+    pdf = require('path').resolve('./src/test/resources/file_uploader_functional_test.pdf');
+    browser.get('http://localhost:5000/#/cadastro');
 });
 
 describe('Register Company', function() {
-  it('should create a new company with partner', function() {
-      var companyName = browser.params.name;
-      var companyCNPJ = browser.params.cnpj;
-      var partnerName = browser.params.partnerName;
-      var partnerCPF = browser.params.cpf;
-      var listCompany = new ListCompany();
+    it('should create a new company with partner', function() {
+        var companyName = browser.params.name;
+        var companyCNPJ = browser.params.cnpj;
+        var partnerName = browser.params.partnerName;
+        var partnerCPF = browser.params.cpf;
+        var listCompany = new ListCompany();
 
-      newCompanyForm.fillFields(companyCNPJ, companyName, pdf);
-      newCompanyForm.fillPartnerFields(partnerName, partnerCPF);
-      newCompanyForm.submit();
+        newCompanyForm.fillFields(companyCNPJ, companyName, pdf);
+        newCompanyForm.fillPartnerFields(partnerName, partnerCPF);
+        newCompanyForm.submit();
 
-      expect(newCompanyForm.isSaved()).toContain('Empresa '+ companyName + ' foi cadastrada.');
+        expect(newCompanyForm.isSaved()).toContain('Empresa '+ companyName + ' foi cadastrada.');
 
-      browser.get('http://localhost:5000/#/empresas');
-      expect(listCompany.containsCompanyName(companyName)).toBe(true);
+        browser.get('http://localhost:5000/#/empresas');
+        expect(listCompany.containsCompanyName(companyName)).toBe(true);
+    });
 
-  });
-
-  it('should create a new company without partner', function() {
+    it('should create a new company without partner', function() {
         var companyName = browser.params.name2;
         var companyCNPJ = browser.params.cnpj2;
         var listCompany = new ListCompany();
@@ -43,19 +42,19 @@ describe('Register Company', function() {
 
     });
 
-  it('should clean form', function() {
-      newCompanyForm.fillFields('16.724.037/0001-00', 'Gama Company LTDA2', pdf);
-      newCompanyForm.clear();
+    it('should clean form', function() {
+        newCompanyForm.fillFields('16.724.037/0001-00', 'Gama Company LTDA2', pdf);
+        newCompanyForm.clear();
 
-      expect(element(by.name('userForm')).getAttribute('class')).not.toMatch('ngDirty');
-  });
+        expect(element(by.name('userForm')).getAttribute('class')).not.toMatch('ngDirty');
+    });
 
-  it('should allow upload the same file after save a company', function() {
-      var verifyButton = element(by.id('verify'));
+    it('should allow upload the same file after save a company', function() {
+        var verifyButton = element(by.id('verify'));
 
-      newCompanyForm.fillFields('20.579.862/0001-28', 'Gama Company LTDA3', pdf);
-      newCompanyForm.uploadDocument(pdf);
+        newCompanyForm.fillFields('20.579.862/0001-28', 'Gama Company LTDA3', pdf);
+        newCompanyForm.uploadDocument(pdf);
 
-      expect(verifyButton.isDisplayed()).toBe(true);
-  });
+        expect(verifyButton.isDisplayed()).toBe(true);
+    });
 });
