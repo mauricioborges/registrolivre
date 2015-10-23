5f791fed2e0db2b6dc7e77241d7eca2e0a104514
Guilherme Dias <gdias@thoughtworks.com>
2015-10-23 19:03:22 -0200

    @guilhermedias #45 use local machine display when running functional tests

--- infrastructure/application/scripts/tests/functional/functional-test.sh
+++ infrastructure/application/scripts/tests/functional/functional-test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ssh -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@192.168.33.71 << EOF
+ssh -Y -i $REGISTROLIVRE_PRIVATE_KEY registrolivre@192.168.33.71 << EOF
 
 clean_database() {
   sudo -u postgres psql registro_livre -c "delete from partners; delete from documents; delete from companies;"
@@ -10,9 +10,14 @@ clean_database
 
 echo "Running functional tests"
 cd /home/registrolivre/registrolivre
-xvfb-run protractor src/test/resources/functional/firefox-conf.js
+protractor src/test/resources/functional/firefox-conf.js
 
 echo "Removing data created by functional tests"
 clean_database
 
+pkill -f dbus-launch
+
 EOF
+
+echo "Killing local X11 server"
+pkill -f Xquartz
