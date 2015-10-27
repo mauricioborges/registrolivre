b7f2b1cc75ad799d9e10962a31aa1f8e75213b9e
Felipe de Morais <fmorais@thoughtworks.com>
2015-10-27 20:08:30 -0200

    @josigama @lipemorais @dieselpardal #44 Makes the controller test work and do some refactoring

--- src/main/resources/static/js/controllers/view-company-controller.js
+++ src/main/resources/static/js/controllers/view-company-controller.js
@@ -1,31 +1,23 @@
 app.controller("CompanyViewController", ["$scope", "$filter", "ngTableParams", "companies", "$routeParams", "$location", "$http",
    function($scope, $filter, ngTableParams, companies, $routeParams, $location, $http) {
 
-    //console.log("AAAAAA:"+JSON.stringify($location));
-   //  console.log("AAAAAA:"+JSON.stringify($routeParams)); // Quero saber o que vem aqui dentro
-    var param1 = $routeParams.companyId;
-    //$scope.cnpj = param1; // numero de Empresa
+    var companyId = $routeParams.companyId;
 
-    getCompanyById(param1).success(function(response)
-    {
-      $scope.cnpj = response.cnpj;
-      $scope.tradeName = response.tradeName;
-      $scope.companyName = response.companyName;
-      $scope.streetName = response.streetName;
-      $scope.number = response.number;
-      $scope.complement = response.complement;
-      $scope.city = response.city;
-      $scope.state = response.state;
-      $scope.cep = response.cep;
-        $scope.openingDate = response.openingDate;
-        $scope.cep = response.cep;
-        $scope.issueDate = response.documents[0].issueDate;
-        $scope.url = response.documents[0].url;
-        $scope.file = response.file;
+    companies.getCompanyById(companyId).success(function(response) {
+        $scope.company = {
+            "id" : response.id,
+            "cnpj" : response.cnpj,
+            "tradeName" : response.tradeName,
+            "companyName" : response.companyName,
+            "streetName" : response.streetName,
+            "number" : response.number,
+            "complement" : response.complement,
+            "city" : response.city,
+            "state" : response.state,
+            "cep" : response.cep,
+            "openingDate" : response.openingDate,
+            "cep" : response.cep,
+            "file" : response.file,
+        };
     });
-
-
-function getCompanyById(companyId) {
-        return $http.get('/empresas/' + companyId);
-    }
 }]);
