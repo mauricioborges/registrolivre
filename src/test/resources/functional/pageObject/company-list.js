875e60f1e681c6e56631a8c60df248bca236bea0
Josi Gama <josyvisa@gmail.com>
2015-10-30 18:18:42 -0200

    @josigama @lipemorais @deniscostadsc @guilhermedias - fix company list page object

--- src/test/resources/functional/pageObject/company-list.js
+++ src/test/resources/functional/pageObject/company-list.js
@@ -1,7 +1,7 @@
 var CompanyList = function () {
 
     this.containsCompanyName = function(name) {
-        return element.all(by.cssContainingText(".text-left > span", name)).first().getText().then(function(_name){
+        return element.all(by.cssContainingText(".text-left > a", name)).first().getText().then(function(_name){
             return name === _name;
         });
     };
