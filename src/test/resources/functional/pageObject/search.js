58a3a07f0ab5ccc93ac6dc69f5c499675e0f7daa
Pedro Silva <pedrro2@hotmail.com>
2015-10-31 14:13:02 -0200

    @pedrro - #27 - add functional tests to basic search

--- src/test/resources/functional/pageObject/search.js
+++ src/test/resources/functional/pageObject/search.js
@@ -3,11 +3,14 @@ var Search = function () {
     this.fillSearchField = function(company) {
           element(by.id("search-input")).sendKeys(company);
     };
-     this.submitSearch = function() {
+
+    this.submitSearch = function() {
           element(by.id("btn-submit")).click();
-     };
+    };
 
-
+    this.isSearchButtonEnable = function() {
+          return element(by.id('search-input')).isEnabled();
+    };
 };
 
 module.exports = Search;
--- src/test/resources/functional/tests/search-company-spec.js
+++ src/test/resources/functional/tests/search-company-spec.js
@@ -7,8 +7,14 @@ var listCompany = new ListCompany();
 describe('Search company',function(){
     it('Should search a company',function(){
       browser.get('http://localhost:5000/#/');
-
       var companyName = browser.params.validCompanyName;
+
+      search.fillSearchField("");
+      expect(search.isSearchButtonEnable()).toBe(false);
+
+      search.fillSearchField("as");
+      expect(search.isSearchButtonEnable()).toBe(true);
+
       search.fillSearchField(companyName);
       search.submitSearch();
 
