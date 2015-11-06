96bf78954dff52a9f40e60b72576b34d089b44a8
Pedro Silva <pedrro2@hotmail.com>
2015-11-06 14:17:07 -0200

    @pedrro | @deniscostadsc - refatorando erro no companyList

--- src/main/resources/static/js/controllers/companies-list-controller.js
+++ src/main/resources/static/js/controllers/companies-list-controller.js
@@ -7,7 +7,7 @@ app.controller('CompaniesListController', ['$scope', '$filter', 'NgTableParams',
     }
 
     function configureTableParams() {
-        $scope.TableParams = new NgTableParams({
+        $scope.tableParams = new NgTableParams({
             page: 1,
             count: 1000,
             sorting: {
