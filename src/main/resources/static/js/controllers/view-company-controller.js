c473ce7400c500622a6f536b3824f05b3e387f5b
Pedro Silva <pedrro2@hotmail.com>
2015-11-10 16:12:22 -0200

    @pedrro - #44 - consertando jshint

--- src/main/resources/static/js/controllers/view-company-controller.js
+++ src/main/resources/static/js/controllers/view-company-controller.js
@@ -1,22 +1,22 @@
-app.controller("CompanyViewController", ["$scope", "$filter", "ngTableParams", "companies", "$routeParams", "$location", "$http",
+app.controller('CompanyViewController', ['$scope', '$filter', 'ngTableParams', 'companies', '$routeParams', '$location', '$http',
    function($scope, $filter, ngTableParams, companies, $routeParams) {
     var companyId = $routeParams.companyId;
     companies.getCompanyById(companyId).success(function(response) {
         $scope.company = {
-            "id" :  response.id,
-            "cnpj" :response.cnpj,
-            "tradeName" : response.tradeName,
-            "companyName" : response.companyName,
-            "streetName" : response.streetName,
-            "number" : response.number,
-            "complement" : response.complement,
-            "city" : response.city,
-            "state" : response.state,
-            "cep" : response.cep,
-            "openingDate" : response.openingDate,
-            "documents": response.documents[0],
-            "file": response.file,
-            "partners": response.partners
+            'id' :  response.id,
+            'cnpj' :response.cnpj,
+            'tradeName' : response.tradeName,
+            'companyName' : response.companyName,
+            'streetName' : response.streetName,
+            'number' : response.number,
+            'complement' : response.complement,
+            'city' : response.city,
+            'state' : response.state,
+            'cep' : response.cep,
+            'openingDate' : response.openingDate,
+            'documents': response.documents[0],
+            'file': response.file,
+            'partners': response.partners
         };
     });
 }]);
@@ -25,7 +25,7 @@ app.filter('showHyphen',
     function () {
         return function (obj) {
            if(obj === null){
-               return "-";
+               return '-';
            }else{
                return obj;
            }
