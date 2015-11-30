4f8c8b37465a45e6ae875d9c447be3023a1deaad
Inajara Leppa <ileppa@thoughtworks.com>
2015-11-30 15:05:59 -0200

    #66 @inajaraleppa e gomex - Criação de um script para configurar o Docker e uma task no build.gradle para rodar esse script

---
+++ infrastructure/application/scripts/deploy/local/deploydocker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cp ../../
+
+docker-machine --version
+if [ $? -eq 0 ];then
+    STATUSDOCKER=`docker-machine status default`
+    if [ $STATUSDOCKER == "Stopped" ];then
+      docker-machine start default
+      if [ "$?" != "0" ];then
+          echo "Default machine not created!!! do it with 'docker-machine create default --driver VirtualBox' and try again"
+          exit 1;
+      fi
+    fi
+    eval $(docker-machine env default)
+
+    echo "=====> Killing containers"
+    docker-compose stop
+    echo "=====> Starting containers"
+    docker-compose up -d
+else
+    echo "Docker Machine not installed."
+    exit 1;
+fi
+
